0686f459b0bc71880213a3241f58de222a695438
SOMUANZA <sozinho.muanza@fas.co.ao>
2026-02-27 06:12:37 +0100

    Corrige App.php para produção Railway

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define a porta padrão do Railway ou 8080
-PORT_NUMBER=${PORT:-8080}
-
-echo "Iniciando PHP na porta $PORT_NUMBER..."
-
-# O parâmetro -d display_errors=Off impede que avisos quebrem os headers
-php -d display_errors=Off -S 0.0.0.0:$PORT_NUMBER -t public
