8f10f4a766360d41da3c6fb95de608c377d8b437
SOMUANZA <sozinho.muanza@fas.co.ao>
2026-02-27 14:25:36 +0100

    diag: verificar estrutura de pastas

---
+++ public/diag2.php
@@ -0,0 +1,10 @@
+﻿<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+echo '<pre>';
+print_r(scandir(__DIR__));
+echo '</pre>';
+echo '<hr>';
+echo '<pre>';
+print_r(scandir(dirname(__DIR__)));
+echo '</pre>';
